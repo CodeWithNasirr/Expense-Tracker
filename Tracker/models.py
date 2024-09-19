ea2b3319ccf38f52a2799340d59424e9e1f11d8f
CodeWithNasirr <skofficial665@gmail.com>
2024-09-19 08:00:50 +0530

    19sepv1

--- Tracker/models.py
+++ Tracker/models.py
@@ -30,7 +30,7 @@ class UserProfile(models.Model):
 
 @receiver(post_save, sender=UserProfile)
 def Avtar_Resizer(sender, instance, created, **kwargs):
-    # sab se pahle avtar check karna ha wo db pe ha ya nhi if it true then save the path in variable just-like(avatar_path) and next open Image From this path when the image open True then Reize it and save into the thumbnail and lastly when the img resize save into the same path if you save the same path it acctully save the same data base path 
+    # sab se pahle avtar check karna ha wo db pe ha ya nhi if it true then save the path in variable just-like(avatar_path) and next open Image From this path when the image open means it  True then Reize it and save into the thumbnail and lastly when the img resize save into the same path if you save in the same path it acctully save the same data base path 
     if instance.avatar:
         avatar_path=instance.avatar.path
         img=Image.open(avatar_path)
@@ -40,5 +40,25 @@ def Avtar_Resizer(sender, instance, created, **kwargs):
             img.thumbnail(max_size)
             img.save(avatar_path)
             print('Resizer Image Saved Succesfully')
+# After a User is created, automatically create a related UserProfile if it doesn’t exist.
+@receiver(post_save,sender=User)
+def create_or_update_user_profile(sender,instance,created,**kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        instance.userprofile.save()
+# After a UserProfile is deleted, remove the avatar image file from the filesystem to free up
+@receiver(post_delete,sender=UserProfile)
+def delete_avatar(sender,instance,**kwargs):
+    if instance.avatar:
+        avatar_path=instance.avatar.path
+        if os.path.exists(avatar_path):
+            os.remove(avatar_path)
+            print(f"Deleted Avatar: {avatar_path}")
+from django.core.exceptions import ValidationError
+@receiver(pre_delete,sender=Transactions)
+def check_Transactons_dependencies(sender,instance,**kwargs):
+    if Transactions.objects.filter(desc=instance).exists():
+        raise ValidationError('Cannot delete desc asscoiated')
 class Register(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
--- Tracker/urls.py
+++ Tracker/urls.py
@@ -3,6 +3,4 @@ from . import views
 urlpatterns=[
     path('',views.Tracker,name='Tracker'),
     path('Delete/<int:id>',views.Delete,name="Deleted"),
-    path('crop-image/', views.crop_image_page, name='crop_image_page'),
-    # path('upload-cropped-image/', views.upload_cropped_image, name='upload_cropped_image'),
 ]
